90576ad434abcd4ede9ce08054ddfc291fef7942
Mostafa Ragab <m.hassan323@alexu.edu.eg>
2023-08-21 03:41:03 +0300

    edit dotenv import

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-import dotenv from "dotenv";
+import path from "path";
+import { config } from "dotenv";
+config({ path: path.resolve("config/.env") });
 import express from "express";
 import { router } from "./src/router.js";
 
-dotenv.config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080;
 
 router(app, express);
 
