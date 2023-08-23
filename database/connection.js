5e5d448c173f39413b1642b59c88dbf55cfcda0f
Mostafa Ragab <m.hassan323@alexu.edu.eg>
2023-08-24 02:59:16 +0300

    connect to DB ATLAS

--- database/connection.js
+++ database/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const db = async () => {
   try {
-    await mongoose.connect(process.env.DB_CONNECTION);
+    await mongoose.connect(process.env.ATLAS_CONNECTION);
     console.log("Database connected:", mongoose.connections[0].name);
   } catch (error) {
     console.log("Error connecting to the database:", error);
