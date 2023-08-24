e83f7b242675bd07fa750d4d48b53238574a314a
Mostafa Ragab <m.hassan323@alexu.edu.eg>
2023-08-24 05:10:44 +0300

    fix DB connectivity issue in admin script

--- src/modules/user/admin.js
+++ src/modules/user/admin.js
@@ -1,22 +1,28 @@
+import path from "path";
+import { config } from "dotenv";
+config({ path: path.resolve("../../../config/.env") });
+import mongoose from "mongoose";
 import { User } from "../../../database/models/user.model.js";
 import { hashPassword } from "../../utils/password/passwordHashing.js";
+import { catchAsyncError } from "../../utils/error/asyncError.js";
 
-export const createAdmin = async () => {
-  try {
-    const hashedPassword = hashPassword(process.env.ADMIN_PASS);
+export const createAdmin = catchAsyncError(async () => {
+  await mongoose.connect(process.env.DB_CONNECTION);
+  console.log("Database connected:", mongoose.connections[0].name);
+  const hashedPassword = hashPassword(process.env.ADMIN_PASS);
 
-    const adminObj = {
-      firstName: "Super",
-      lastName: "Admin",
-      email: process.env.EMAIL_ADDRESS,
-      password: hashedPassword,
-      role: "admin",
-      verifiedEmail: true,
-    };
+  const adminObj = {
+    firstName: "Super",
+    lastName: "Admin",
+    email: process.env.EMAIL_ADDRESS,
+    password: hashedPassword,
+    role: "admin",
+    verifiedEmail: true,
+  };
 
-    await User.create(adminObj);
-    console.log("Admin account created successfully");
-  } catch (error) {
-    console.error("Error creating admin:", error);
-  }
-};
+  await User.create(adminObj);
+  console.log("Admin account created successfully");
+  mongoose.disconnect();
+});
+
+createAdmin();
