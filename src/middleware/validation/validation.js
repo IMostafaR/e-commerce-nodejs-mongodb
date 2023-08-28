df7e947319c40e530d03c55840a69e9291b52160
Mostafa Ragab <m.hassan323@alexu.edu.eg>
2023-08-28 23:32:10 +0300

    handle mainValidationSchema joi error message

--- src/middleware/validation/validation.js
+++ src/middleware/validation/validation.js
@@ -2,30 +2,35 @@ import Joi from "joi";
 import { AppError } from "../../utils/error/appError.js";
 
 export const mainValidationSchema = {
-  firstName: Joi.string().min(2).max(16).required().message({
+  firstName: Joi.string().min(2).max(16).required().messages({
+    "string.empty": `firstName cannot be an empty field`,
     "string.min": "firstName must be at least 2 characters long",
     "string.max": "firstName cannot be more than 16 characters long",
     "any.required": "firstName is required",
   }),
-  lastName: Joi.string().min(2).max(16).required().message({
+  lastName: Joi.string().min(2).max(16).required().messages({
+    "string.empty": `lastName cannot be an empty field`,
     "string.min": "lastName must be at least 2 characters long",
     "string.max": "lastName cannot be more than 16 characters long",
     "any.required": "lastName is required",
   }),
-  email: Joi.string().email({ minDomainSegments: 3 }).required().message({
+  email: Joi.string().email({ minDomainSegments: 2 }).required().messages({
+    "string.empty": `Email cannot be an empty field`,
     "string.email": "Invalid email",
     "any.required": "Email is required",
   }),
   password: Joi.string()
     .pattern(/^[a-zA-Z0-9]{3,30}$/)
     .required()
-    .message({
+    .messages({
+      "string.empty": `Password cannot be an empty field`,
       "string.pattern.base": "Password must contain only letters and numbers",
       "any.required": "Password is required",
     }),
-  repeatPassword: Joi.string().valid(Joi.ref("password")).required().message({
+  repeatPassword: Joi.string().valid(Joi.ref("password")).required().messages({
+    "string.empty": `Repeat password cannot be an empty field`,
     "any.only": "Repeat password must match the password",
-    "any.required": "Repeat password is required",
+    "any.required": "Password is required",
   }),
   phone: Joi.string()
     .pattern(/^01[0125][0-9]{8}$/)
