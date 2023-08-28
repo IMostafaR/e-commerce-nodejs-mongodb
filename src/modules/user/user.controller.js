5545664b77b909678e5df7106d48f720bb9d703e
Mostafa Ragab <m.hassan323@alexu.edu.eg>
2023-08-28 08:04:36 +0300

    initiate user signup controller

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -1,6 +1,7 @@
 import { catchAsyncError } from "../../utils/error/asyncError.js";
 
 export const user = {
+  //signup
   signup: catchAsyncError(async (req, res, next) => {
     const { firstName, lastName, email, password, repeatPassword, phone } =
       req.body;
