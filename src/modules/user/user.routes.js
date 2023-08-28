66f1db3e6b94c89286bb5824d67c04873f70e8c5
Mostafa Ragab <m.hassan323@alexu.edu.eg>
2023-08-28 23:36:24 +0300

    import validation function into user router

--- src/modules/user/user.routes.js
+++ src/modules/user/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { user } from "./user.controller.js";
 import { userValidatorSchema } from "./user.validator.js";
+import { validation } from "../../middleware/validation/validation.js";
 
 export const userRouter = Router();
 
