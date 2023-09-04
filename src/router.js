6cacca404aa40ecccc394fe735672948e7fee717
Mostafa Ragab <m.hassan323@alexu.edu.eg>
2023-09-05 01:42:47 +0300

    initiate subcategory route and files

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import { db } from "../database/connection.js";
 import { globalErrorHandler } from "./middleware/error/globalErrorHandler.js";
 import { categoryRouter } from "./modules/category/category.routes.js";
+import { subcategoryRouter } from "./modules/subcategory/subcategory.routes.js";
 import { userRouter } from "./modules/user/user.routes.js";
 import { AppError } from "./utils/error/appError.js";
 import morgan from "morgan";
@@ -18,6 +19,7 @@ export const router = (app, express) => {
   app.use(morgan("dev"));
   app.use("/api/v1/user", userRouter);
   app.use("/api/v1/categories", categoryRouter);
+  app.use("/api/v1/subcategories", subcategoryRouter);
   app.use("/uploads", express.static("uploads"));
   app.all("*", (req, res, next) => {
     next(new AppError(`invalid routing ${req.originalUrl}`, 404));
