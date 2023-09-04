ec0254bbb747fe9c9b7dd640be0e0f1cc4fdd87a
Mostafa Ragab <m.hassan323@alexu.edu.eg>
2023-09-05 01:45:08 +0300

    subcategoryRouter routes init

--- src/modules/subcategory/subcategory.routes.js
+++ src/modules/subcategory/subcategory.routes.js
@@ -1,3 +1,7 @@
 import { Router } from "express";
 
 export const subcategoryRouter = Router();
+
+subcategoryRouter.route("/").get().post();
+
+subcategoryRouter.route("/:id").get().put().delete();
