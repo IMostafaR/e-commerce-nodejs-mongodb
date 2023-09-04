81a5c79ff69907c4672ab7641b0b3d58cbcc5176
Mostafa Ragab <m.hassan323@alexu.edu.eg>
2023-09-04 04:50:49 +0300

    remove comments

--- src/modules/category/category.routes.js
+++ src/modules/category/category.routes.js
@@ -9,13 +9,7 @@ export const categoryRouter = Router();
 
 categoryRouter
   .route("/")
-  /**
-   * Get all categories from DB
-   */
   .get(category.getAll)
-  /**
-   * create new category route
-   */
   .post(
     uploadFileCloud({
       fileType: fileValidation.image,
@@ -25,20 +19,11 @@ categoryRouter
 
 categoryRouter
   .route("/:id")
-  /**
-   * Get a specific category with its id from DB
-   */
   .get(category.getOne)
-  /**
-   * update existing category route
-   */
   .put(
     uploadFileCloud({
       fileType: fileValidation.image,
     }).single("image"),
     category.update
   )
-  /**
-   * Delete a specific category by its id from DB
-   */
   .delete(category.deleteOne);
