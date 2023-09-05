92e6910afa3103af09e6a61e123aee615d02c524
Mostafa Ragab <m.hassan323@alexu.edu.eg>
2023-09-05 22:35:37 +0300

    add JSDoc comment

--- src/utils/error/appError.js
+++ src/utils/error/appError.js
@@ -1,6 +1,20 @@
+/**
+ * Class to create new error
+ */
 export class AppError extends Error {
+  /**
+   * Error details
+   * @param {string} message
+   * @param {number} statusCode
+   */
   constructor(message, statusCode) {
+    /**
+     * @property {string} message
+     */
     super(message);
+    /**
+     * @property {number} statusCode
+     */
     this.statusCode = statusCode;
   }
 }
