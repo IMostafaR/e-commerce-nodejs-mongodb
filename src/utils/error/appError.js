c660cf5b5ebca723daf7bb147743e2431d6655a7
Mostafa Ragab <m.hassan323@alexu.edu.eg>
2023-09-07 04:51:17 +0300

    edit comments of appError class

--- src/utils/error/appError.js
+++ src/utils/error/appError.js
@@ -1,19 +1,22 @@
 /**
- * Class to create new error
+ * Custom error class for application-specific errors.
  */
 export class AppError extends Error {
   /**
-   * Error details
-   * @param {string} message
-   * @param {number} statusCode
+   * Creates a new application-specific error.
+   *
+   * @param {string} message - A descriptive error message.
+   * @param {number} statusCode - The HTTP status code to be set when handling this error.
    */
   constructor(message, statusCode) {
     /**
-     * @property {string} message
+     * A descriptive error message.
+     * @type {string}
      */
     super(message);
     /**
-     * @property {number} statusCode
+     * The HTTP status code associated with the error.
+     * @type {number}
      */
     this.statusCode = statusCode;
   }
