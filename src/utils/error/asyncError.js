ea37ebce3864af4d125250a9799a2bdb758d314b
Mostafa Ragab <m.hassan323@alexu.edu.eg>
2023-09-07 04:48:31 +0300

    adding comments to catchAsyncError

--- src/utils/error/asyncError.js
+++ src/utils/error/asyncError.js
@@ -1,3 +1,9 @@
+/**
+ * Wraps an asynchronous function to catch and forward errors to the next middleware.
+ *
+ * @param {Function} fn - The asynchronous function to wrap.
+ * @returns {Function} - A middleware function that catches errors and forwards them to the next middleware.
+ */
 export const catchAsyncError = (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch((error) => {
