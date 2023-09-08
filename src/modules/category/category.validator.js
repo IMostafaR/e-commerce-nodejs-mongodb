6ca2b88ccfc778042b8c10889185dc47ffb2a286
Mostafa Ragab <m.hassan323@alexu.edu.eg>
2023-09-08 08:03:09 +0300

    add comment

--- src/modules/category/category.validator.js
+++ src/modules/category/category.validator.js
@@ -1,6 +1,9 @@
 import Joi from "joi";
 import { mainValidationSchema } from "../../middleware/validation/validation.js";
 
+/**
+ * create new category validation schema
+ */
 const createCategory = Joi.object({
   name: mainValidationSchema.name,
 }).options({ allowUnknown: true });
