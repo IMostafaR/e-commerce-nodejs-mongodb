70cfb4204011a519e65845a88301f7694ad1c7bc
Mostafa Ragab <m.hassan323@alexu.edu.eg>
2023-09-25 14:13:13 +0300

    createReview controller

---
+++ src/modules/review/review.controller.js
@@ -0,0 +1,45 @@
+import { Review } from "../../../database/models/review.model.js";
+import { AppError } from "../../utils/error/appError.js";
+import { catchAsyncError } from "../../utils/error/asyncError.js";
+
+/**
+ * Create a new review based on the provided request body fields.
+ * @function
+ * @async
+ * @param {express.Request} req - The Express request object.
+ * @param {express.Response} res - The Express response object.
+ * @param {express.NextFunction} next - The Express next middleware function.
+ *
+ * @throws {AppError} If the customer has already reviewed this product, it returns a 409 Conflict error.
+ *
+ * @returns {Promise<void>} A promise that resolves when the operation is complete.
+ */
+const createReview = catchAsyncError(async (req, res, next) => {
+  const { title, content, rate } = req.body;
+  const { id: product } = req.params;
+  const { id: customer } = req.user;
+
+  // check if the customer has already reviewed this product
+  const existingReview = await Review.findOne({ customer, product });
+  if (existingReview) {
+    return next(new AppError("You've already reviewed this product", 409));
+  }
+
+  // Create a new review and save it into the database
+  const newReview = await Review.create({
+    title,
+    content,
+    rate,
+    product,
+    customer,
+  });
+
+  // Send a successful response
+  res.status(201).json({
+    status: "success",
+    message: "Review successfully added.",
+    data: newReview,
+  });
+});
+
+export { createReview };
