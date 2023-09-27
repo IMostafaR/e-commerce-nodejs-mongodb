a5fbb2454e52f9e489fa3cb49675b52b03ff5dfc
Mostafa Ragab <m.hassan323@alexu.edu.eg>
2023-09-27 14:45:21 +0300

    edit createProductValidation schema and add custom validation for discount and soldItems

--- src/modules/product/product.validator.js
+++ src/modules/product/product.validator.js
@@ -9,21 +9,33 @@ const createProductValidation = Joi.object({
     "string.max": "description cannot be more than 200 characters long",
     "any.required": "description is required",
   }),
-  price: Joi.number().min(0).messages({
+
+  price: Joi.number().min(0).required().messages({
     "number.empty": "price cannot be an empty field",
     "number.min": "price cannot be less than zero",
+    "any.required": "price is required",
   }),
-  discount: Joi.number().min(0).messages({
-    "number.empty": "discount cannot be an empty field",
-    "number.min": "discount cannot be less than zero",
-  }),
-  quantity: Joi.number().min(0).messages({
+  discount: Joi.number()
+    .min(0)
+    .required()
+    .messages({
+      "number.custom": "Invalid discount value",
+      "any.required": "discount is required",
+    })
+    .messages({
+      "number.empty": "discount cannot be an empty field",
+      "number.min": "discount cannot be less than zero",
+      "any.required": "discount is required",
+    }),
+  quantity: Joi.number().min(0).required().messages({
     "number.empty": "quantity cannot be an empty field",
     "number.min": "quantity cannot be less than zero",
+    "any.required": "quantity is required",
   }),
-  soldItems: Joi.number().min(0).messages({
+  soldItems: Joi.number().min(0).required().messages({
     "number.empty": "sold items cannot be an empty field",
     "number.min": "sold items cannot be less than zero",
+    "any.required": "sold items is required",
   }),
   ratingAvg: Joi.number().min(1).max(5).messages({
     "number.empty": "ratingAvg cannot be an empty field",
@@ -33,9 +45,25 @@ const createProductValidation = Joi.object({
   category: mainValidationSchema.id,
   subcategory: mainValidationSchema.id,
   brand: mainValidationSchema.id,
-  //   createdBy: mainValidationSchema.id,
-  //   updatedBy: mainValidationSchema.id,
-}).options({ allowUnknown: true });
+  token: mainValidationSchema.token,
+}).custom((values, helpers) => {
+  const validDiscount = values.discount <= values.price;
+  const validSoldItems = values.soldItems <= values.quantity;
+
+  if (!validDiscount && !validSoldItems) {
+    return helpers.message(
+      "Discount should be less than the price of the product and sold items should be less than the quantity of the product"
+    );
+  } else if (!validDiscount) {
+    return helpers.message(
+      "Discount should be less than the price of the product"
+    );
+  } else if (!validSoldItems) {
+    return helpers.message(
+      "Sold items should be less than the quantity of the product"
+    );
+  }
+}, "custom validation");
 
 const handleOneProductValidation = Joi.object({
   id: mainValidationSchema.id,
