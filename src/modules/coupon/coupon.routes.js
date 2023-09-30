e4b453983a8be8784f002273947e1f433f60df6a
Mostafa Ragab <m.hassan323@alexu.edu.eg>
2023-09-30 19:04:10 +0300

    createCoupon route

--- src/modules/coupon/coupon.routes.js
+++ src/modules/coupon/coupon.routes.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
+import { authenticate, authorize } from "../auth/auth.controller.js";
+import { createCoupon } from "./coupon.controller.js";
 
 export const couponRouter = Router();
 
-couponRouter.route("/");
+couponRouter.route("/").post(authenticate, authorize("admin"), createCoupon);
