540c6e9b8fcadb5a8a9b0100dcfd02ffd91a6d3d
Mostafa Ragab <m.hassan323@alexu.edu.eg>
2023-10-05 18:35:04 +0300

    and name field to product doc

--- database/models/order.model.js
+++ database/models/order.model.js
@@ -38,6 +38,10 @@ const OrderSchema = new mongoose.Schema(
           ref: "Product",
           required: true,
         },
+        name: {
+          type: String,
+          required: true,
+        },
         quantity: {
           type: Number,
           required: true,
