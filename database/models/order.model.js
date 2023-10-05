94ed347af144af32f68b4c3ad50745b61b08af25
Mostafa Ragab <m.hassan323@alexu.edu.eg>
2023-10-05 14:20:14 +0300

    order model init

---
+++ database/models/order.model.js
@@ -0,0 +1,78 @@
+import mongoose from "mongoose";
+
+const OrderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    address: {
+      _id: false,
+      street: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      zipCode: {
+        type: Number,
+        required: true,
+      },
+      country: {
+        type: String,
+        required: true,
+      },
+      phone: {
+        type: String,
+        required: true,
+      },
+    },
+    products: [
+      {
+        _id: false,
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    coupon: {
+      type: {
+        _id: false,
+        code: {
+          type: String,
+        },
+        discount: {
+          type: Number,
+        },
+      },
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "completed", "cancelled"],
+    },
+    paymentMethod: {
+      type: String,
+      enum: ["cash", "card"],
+    },
+  },
+  { timestamps: true }
+);
+
+export const Order = mongoose.model("Order", OrderSchema);
