889b365bdefa076f76641181fa45df4eb72ab276
Mostafa Ragab <m.hassan323@alexu.edu.eg>
2023-10-06 22:42:22 +0300

    install cors

--- src/router.js
+++ src/router.js
@@ -1,4 +1,5 @@
 import { db } from "../database/connection.js";
+import cors from "cors";
 import { globalErrorHandler } from "./middleware/error/globalErrorHandler.js";
 import { addressRouter } from "./modules/address/address.routes.js";
 import { authRouter } from "./modules/auth/auth.routes.js";
@@ -24,6 +25,7 @@ export const router = (app, express) => {
   });
 
   db();
+  app.use(cors());
   app.use(express.json());
   app.use(morgan("dev"));
   app.use("/api/v1/auth", authRouter);
